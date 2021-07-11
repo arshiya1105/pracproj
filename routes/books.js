247f73a16025577fb7223f8a415bb5bdfc05698a
arshiya <arshiya.ux@gmail.com>
2021-07-11 22:08:49 +0530

    done with validator sepearator

--- routes/books.js
+++ routes/books.js
@@ -23,108 +23,108 @@ router.get('/add', function(req, res, next) {
         Description:''       
     })
 })
-// router.post('/add', function(req, res, next) {    
+router.post('/add', function(req, res, next) {    
 
-//     let name = req.body.name;
-//     let author = req.body.author;
-//     let Description = req.body.Description;
+    let name = req.body.name;
+    let author = req.body.author;
+    let Description = req.body.Description;
 
-//     let errors = false;
+    let errors = false;
 
-//     if(name.length === 0 || author.length === 0) {
-//         errors = true;
+    if(name.length === 0 || author.length === 0) {
+        errors = true;
 
-//         // set flash message
-//         req.flash('error', "Please enter name and author");
-//         // render to add.ejs with flash message
-//         res.render('books/add', {
-//             name: name,
-//             author: author,
-//             Description:Description
-//         })
-//     }
+        // set flash message
+        req.flash('error', "Please enter name and author");
+        // render to add.ejs with flash message
+        res.render('books/add', {
+            name: name,
+            author: author,
+            Description:Description
+        })
+    }
 
-//     // if no error
-//     if(!errors) {
+    // if no error
+    if(!errors) {
 
-//         var form_data = {
-//             name: name,
-//             author: author,
-//             Description:Description
-//         }
+        var form_data = {
+            name: name,
+            author: author,
+            Description:Description
+        }
         
-//         // insert query
-//         dbConn.query('INSERT INTO books SET ?', form_data, function(err, result) {
-//             //if(err) throw err
-//             if (err) {
-//                 req.flash('error', err)
+        // insert query
+        dbConn.query('INSERT INTO books SET ?', form_data, function(err, result) {
+            //if(err) throw err
+            if (err) {
+                req.flash('error', err)
                  
-//                 // render to add.ejs
-//                 res.render('books/add', {
-//                     name: form_data.name,
-//                     author: form_data.author,
-//                     Description:form_data.Description                   
-//                 })
-//             } else {                
-//                 req.flash('success', 'Book successfully added');
-//                 res.redirect('/books');
-//             }
-//         })
-//     }
-// })
+                // render to add.ejs
+                res.render('books/add', {
+                    name: form_data.name,
+                    author: form_data.author,
+                    Description:form_data.Description                   
+                })
+            } else {                
+                req.flash('success', 'Book successfully added');
+                res.redirect('/books');
+            }
+        })
+    }
+})
 
-router.post('/add',
-    [
-        check('name').not().isEmpty().withMessage('Name field requeried'),
-        check('author', 'author value must be neaded').not().isEmpty(),
-        check('Description').not().isEmpty().isLength({min: 5}).withMessage('Name must have more than 5 characters'),
+// router.post('/add',
+//     [
+//         check('name').not().isEmpty().withMessage('Name field r'),
+//         check('author', 'author value must be neaded').not().isEmpty(),
+//         check('Description').not().isEmpty().isLength({min: 5}).withMessage('Name must have more than 5 characters'),
 
-    ],
+//     ],
  
-  function (req, res) {
-    const errors = validationResult(req);
+//   function (req, res) {
+//     const errors = validationResult(req);
   
-        if (!errors.isEmpty()) {
+//         if (!errors.isEmpty()) {
 
-            req.flash('error',  errors.array());
-            // render to add.ejs with flash message
-            res.render('books/add', {
-                name: req.body.name,
-                author: req.body.author,
-                Description:req.body.Description
-            })
+//             req.flash('error',  errors.array());
+//             // render to add.ejs with flash message
+//             res.render('books/add', {
+//                 name: req.body.name,
+//                 author: req.body.author,
+//                 Description:req.body.Description
+//             })
 
            
-          }else{
-            var form_data = {
-                        name: req.body.name,
-                        author: req.body.author,
-                        Description:req.body.Description
-                        }
+//           }else{
+//             var form_data = {
+//                         name: req.body.name,
+//                         author: req.body.author,
+//                         Description:req.body.Description
+//                         }
                         
-                        // insert query
-                        dbConn.query('INSERT INTO books SET ?', form_data, function(err, result) {
-                            //if(err) throw err
-                            if (err) {
-                                req.flash('error', err)
+//                         // insert query
+//                         dbConn.query('INSERT INTO books SET ?', form_data, function(err, result) {
+//                             //if(err) throw err
+//                             if (err) {
+//                                 req.flash('error', err)
                                  
-                                // render to add.ejs
-                                res.render('books/add', {
-                                    name: form_data.name,
-                                    author: form_data.author,
-                                    Description:form_data.Description                   
-                                })
-                            } else {                
-                                req.flash('success', 'Book successfully added');
-                                res.redirect('/books');
-                            }
-                        })
-          }
+//                                 // render to add.ejs
+//                                 res.render('books/add', {
+//                                     name: form_data.name,
+//                                     author: form_data.author,
+//                                     Description:form_data.Description                   
+//                                 })
+//                             } else {                
+//                                 req.flash('success', 'Book successfully added');
+//                                 res.redirect('/books');
+//                             }
+//                         })
+//           }
    
 
 
 
-  });
+//   });
 
 
 router.get('/edit/(:id)', function(req, res, next) {
--- routes/student.js
+++ routes/student.js
@@ -13,97 +13,78 @@ router.get('/', function(req, res, next) {
         }else {
         res.render('student',{data:rows});
         }
-    })})
-   
-   
 
-    router.post('/sadd', (req,res)=>{
-        let mungourl ="mongodb+srv://mungoUserKPVPcl:ZKDf8QmMXRIFekgE@kpvkc1.k30oo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-     
-        try {
-        MongoClient.connect(mungourl, {useNewUrlParser: true, useUnifiedTopology: true}, function(err,db){
-            if (err) throw err;
-            var dbo=db.db("StudentsDataBase");
-            dbo.collection("StudentsList").insertOne( {              FirstName: req.body.FirstName,
-                    LasttName: req.body.LasttName ,
-                    ParentName:req.body.ParentName,
-                    Email:req.body.Email,
-                    PhoneNumber:req.body.PhoneNumber
-      },
-                (err, result)=>{
-                    if (err) throw err;
-                    console.log(req.body)
-                     res.send("user details submitted sucefully");
-                   })
     })
-    } catch (error) {
-        res.status(500).send(error);
-    }
-       
-    })
+})
+
+router.get('/sadd', function(req, res, next) {    
     
-//     router.post('/sadd',
-//     [
-//         check('FirstName').not().isEmpty().withMessage('FirstName field requeried'),
-//         check('LasttName').not().isEmpty().withMessage('LasttName field requeried'),
-//         check('ParentName').not().isEmpty().withMessage('ParentName field requeried'),
-//         check('Email', 'Email value must be neaded').not().isEmpty(),
-//         check('PhoneNumber').not().isEmpty().isLength({min: 5}).withMessage('PhoneNumber munst be interger'),
-
-//     ],
- 
-//   function (req, res) {
-//     const errors = validationResult(req);
+    res.render('student/sadd', {
+        FirstName: '',
+        LasttName: '' ,
+        ParentName:'',
+        Email:'' ,      
+        PhoneNumber:''       
+      
+    })
+})
+var validator=[check('FirstName').not().isEmpty().withMessage('FirstName field requeried'),
+        check('LasttName').not().isEmpty().withMessage('LasttName field requeried'),
+        check('ParentName').not().isEmpty().withMessage('ParentName field requeried'),
+        check('Email', 'Email value must be neaded').not().isEmpty(),
+        check('PhoneNumber').not().isEmpty().isLength({min: 5}).withMessage('PhoneNumber munst be interger'),
+]
+router.post('/sadd',validator, function (req, res) {
+    const errors = validationResult(req);
   
-//         if (!errors.isEmpty()) {
+        if (!errors.isEmpty()) {
 
-//             req.flash('error',  errors.array());
-//             // render to add.ejs with flash message
-//             res.render('books/add', {
-//                 FirstName: req.body.FirstName,
-//                 LasttName: req.body.LasttName ,
-//                 ParentName:req.body.ParentName,
-//                 Email:req.body.Email,
-//                 PhoneNumber:req.body.PhoneNumber
+            req.flash('error',  errors.array());
+            // render to add.ejs with flash message
+            res.render('student/sadd', {
+                FirstName: req.body.FirstName,
+                LasttName: req.body.LasttName ,
+                ParentName:req.body.ParentName,
+                Email:req.body.Email,
+                PhoneNumber:req.body.PhoneNumber
                
-//             })
+            })
 
            
-//           }else{
-//             var form_data = {
-//                 FirstName: req.body.FirstName,
-//                 LasttName: req.body.LasttName ,
-//                 ParentName:req.body.ParentName,
-//                 Email:req.body.Email,
-//                 PhoneNumber:req.body.PhoneNumber
-//                         }
+          }else{
+            var form_data = {
+                FirstName: req.body.FirstName,
+                LasttName: req.body.LasttName ,
+                ParentName:req.body.ParentName,
+                Email:req.body.Email,
+                PhoneNumber:req.body.PhoneNumber
+                        }
                         
-//                         // insert query
-//                         dbConn.query('INSERT INTO students SET ?', form_data, function(err, result) {
-//                             //if(err) throw err
-//                             if (err) {
-//                                 req.flash('error', err)
+                        // insert query
+                        dbConn.query('INSERT INTO students SET ?', form_data, function(err, result) {
+                            //if(err) throw err
+                            if (err) {
+                                req.flash('error', err)
                                  
-//                                 // render to add.ejs
-//                                 res.render('student/sadd', {
-//                                     FirstName: req.body.FirstName,
-//                                     LasttName: req.body.LasttName ,
-//                                     ParentName:req.body.ParentName,
-//                                     Email:req.body.Email,
-//                                     PhoneNumber:req.body.PhoneNumber                 
-//                                 })
-//                             } else {                
-//                                 req.flash('success', 'Book successfully added');
-//                                 res.redirect('/student');
-//                             }
-//                         })
-//           }
+                                // render to add.ejs
+                                res.render('student/sadd', {
+                                    FirstName: req.body.FirstName,
+                                    LasttName: req.body.LasttName ,
+                                    ParentName:req.body.ParentName,
+                                    Email:req.body.Email,
+                                    PhoneNumber:req.body.PhoneNumber                 
+                                })
+                            } else {                
+                                req.flash('success', 'Book successfully added');
+                                res.redirect('/student');
+                            }
+                        })
+          }
    
 
 
 
-//   });
+  });
 
-    
 
 module.exports = router;
